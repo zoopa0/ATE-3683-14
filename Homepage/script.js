f3b852ccdecc105902ef8f3099596232fa6b8739
Ayansa Adugna <136491655+zoopa0@users.noreply.github.com>
2024-05-11 23:47:38 +0300

    Add files via upload

---
+++ Homepage/script.js
@@ -0,0 +1,51 @@
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+        currentIndex = 0;
+    } else {
+        currentIndex = index;
+    }
+
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+    // Update active dot
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[currentIndex].classList.add('active');
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+function currentSlide(index) {
+    showSlide(index - 1);
+}
+
+// Automatic slide change
+setInterval(nextSlide, 5000); // Change slide every 5 seconds
+//for the burger icon
+
+
+function toggleMenu() {
+    const burgerIcon = document.querySelector('.navList');
+    burgerIcon.classList.toggle('open');
+}
+
+//this is for the login button"
+document.getElementsByClass("loginbtn").addEventListener("click", function() {
+    window.location.href = "loginbtn\login\login.html"; 
+});
+
+
+
